d4fb2e5d4bd9f10470560530d506b46edd628af9
reachforthestars551 <32173191+reachforthestars551@users.noreply.github.com>
2017-09-26 16:28:18 -0500

    i forgot a fucken bracket i wanna die

--- rob.js
+++ rob.js
@@ -9,13 +9,16 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong');
-    
+  }
+}
+
 // new member
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'member-log');
   if (!channel) return;
-  channel.send(`Welcome to Robotics, ${member}! Remember to read the rules!`);
-  
+  channel.send(`Welcome to Robotics, ${member}! Remember to read the rules!`)
+};
+
   // avatar
 client.on('message', message => {
   if (message.content === 'avatar?') {
