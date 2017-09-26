1a6482727de6b29efb1024df82f4092516b3f775
reachforthestars551 <32173191+reachforthestars551@users.noreply.github.com>
2017-09-26 14:55:52 -0500

    clean up and avatar command added

--- rob.js
+++ rob.js
@@ -4,16 +4,22 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('I am ready!');
 });
+
 //ping pong
 client.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong');
+    
 // new member
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'member-log');
   if (!channel) return;
   channel.send(`Welcome to Robotics, ${member}! Remember to read the rules!`);
-});
+  
+  // avatar
+client.on('message', message => {
+  if (message.content === 'avatar?') {
+    message.reply(message.author.avatarURL);
   }
 });
 
